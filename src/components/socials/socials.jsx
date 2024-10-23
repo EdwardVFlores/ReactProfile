7ac6a9dfdcf130d4f7e323a5a564e00f446fbe6f
Edward Flores <edwardvflores1@gmail.com>
2024-10-23 23:22:48 +0300

    Socials fixed

--- src/components/socials/socials.jsx
+++ src/components/socials/socials.jsx
@@ -7,7 +7,7 @@ const Socials = () => {
     return (
         <div id="socials" className="socials-container-holder">
             <div className="socials-container">
-                <h2 className="socals-tiitle">Socials</h2>
+                <h2 className="socials-title">Socials</h2>
                 <div className="socials-button-container">
                     <PrimaryButton text="Linked In" link="https://www.linkedin.com/in/edward-flores-b27350178/"/>
                     <PrimaryButton text="A really really long text" link="no idea"/>
